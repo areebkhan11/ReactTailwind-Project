b71254b5342c2a51e6f346e7f17f5fcc334f1ac3
Areeb Khan <areebkhan123123@gmail.com>
2022-10-06 15:54:32 +0500

    refactor

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -11,18 +11,36 @@ import img from "../../assets/images/kisspng-avatar-user-medicine-surgery-patien
 
 
 const NavbarItems =  [
-    { name: "Dashboard", link: '/dashboard', icon: MdOutlineSpaceDashboard },
-    { name: "Users", link: "/users", icon: MdSupervisedUserCircle },
-    { name: "Branch", link: "/branch", icon: FiGitBranch },
-    { name: "Accademic Session", link: "/accademic-session", icon: GoCalendar },
-    { name: "Setting", link: "/setting", icon: MdSettings }
+    
+    {id: 0, name: "Dashboard", link: '/dashboard', icon: MdOutlineSpaceDashboard },
+    {id: 1, name: "Users", link: "/users", icon: MdSupervisedUserCircle },
+    {id: 2, name: "Branch", link: "/branch", icon: FiGitBranch },
+    {id: 3, name: "Accademic Session", link: "/accademic-session", icon: GoCalendar },
+    {id: 4, name: "Setting", link: "/setting", icon: MdSettings },
+    {id: 5, multi: true, name: "Calanders", list: [{ name: "First value", link:"/main", icon: GoCalendar}]  },
+    {id: 6, multi: true, name: "Reports", list: [{ name: "second value", link:"/main", icon: GoCalendar},
+                                                  { name: "third value", link:"/main", icon: GoCalendar}]  }
 ]
    
 
 
 export default function Sidebar() {
   const [open, setopen] = React.useState(false)
+  const [dropdown, setDropdown] = React.useState(false)
+  const [id, setId] = React.useState(false)
   const navigate = useNavigate();
+
+
+  const handletogle = (i) =>{
+
+      const filter = NavbarItems.find((x=> x.id === i))
+      if(filter.id === i){
+        setId(i)
+        setDropdown((dropdown) => !dropdown)
+      }
+
+ 
+  }
  
     return (
       <>
@@ -51,44 +69,46 @@ export default function Sidebar() {
           {
             NavbarItems?.map((e, i) => {
               return (
-
-                <a onClick={()=>navigate(e.link)}  key={i} className="group px-4 cursor-pointer flex items-center gap-6 h-8 target:shadow-lg  hover:bg-blue-500 rounded-r-full active:ring-cyan-200  focus:ring-2 focus:ring-cyan-200">
+                <>
+               {!e?.multi &&
+                <a onClick={()=>navigate(e.link)}  key={i} className="group px-5 cursor-pointer flex items-center gap-6 h-8 target:shadow-lg  hover:bg-blue-500 rounded-r-full active:ring-cyan-200  focus:ring-2 focus:ring-cyan-200">
                   <div>{React.createElement(e.icon, { size: '17' })}</div>
                   <h6 className={`whitespace-pre duration-500 ${!open && "opacity-0 translate-x-28 overflow-hidden"}`}>{e.name}</h6>
-                </a>
+                </a>}
+
+                {e?.multi && <ul className="relative">
+                  <li>
+                    <button onClick={()=>handletogle(i)} type="button"  className="group px-4 cursor-pointer flex items-center gap-6 target:shadow-lg  " aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <div>{React.createElement(GoCalendar, { size: '17' })}</div>
+                    <h6 className={`whitespace-pre duration-500 ${!open && "opacity-0 translate-x-28 overflow-hidden"}`}>{e.name}</h6>
+                    <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        
+                    </button>
+                      {id === i && e?.list.map((v, i)=>{
+                        return(
+                        <ul id="dropdown-example" className={dropdown ? "py-2 space-y-2" : "hidden py-2 space-y-2"}>
+                          <li>
+                            <a   href="#" className="flex items-center w-full p-1 text-base font-normal text-white transition duration-75 rounded-lg group hover:bg-blue-500 rounded-r-full active:ring-cyan-200  focus:ring-2 focus:ring-cyan-200 pl-20">{v.name}</a>
+                          </li>
+                        </ul>
+                        )
+
+                      })
+
+
+            }
+                  </li>
+                </ul>}
                 
-                
+                </>
                 )
 
             })
-
           }
+          
+          {/* Multi level dropdown */}
           {
-            <ul class="relative">
-            <li>
-					<button type="button" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                  <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-                  <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>E-commerce</span>
-                  <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
-					<ul id="dropdown-example" class="hidden py-2 space-y-2">
-						<li>
-							<a href="#"
-								class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Products</a>
-						</li>
-						<li>
-							<a href="#"
-								class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Billing</a>
-						</li>
-						<li>
-							<a href="#"
-								class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Invoice</a>
-						</li>
-					</ul>
-				</li>
-             
-           
-          </ul>
+            
           }
 
         </div>
